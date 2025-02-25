bb66e1ba3c1ef5d77d83ffdebe554253cd7002ac
Nasr <larko236@outlook.com>
2025-02-25 12:32:03 +0800

    mcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use async_mcp::{run_http_server, server::Server, types::ServerCapabilities};
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        // needs to be stderr due to stdio transport
+        .with_writer(std::io::stderr)
+        .init();
+
+    run_http_server(4040, None, |transport| async move {
+        let server = Server::builder(transport).capabilities(ServerCapabilities {
+            tools: Some(json!({})),
+            ..Default::default()
+        }).build();
+        Ok(server)
+    })
+    .await?;
+
+    Ok(())
 }
